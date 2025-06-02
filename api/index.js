0bad83d2d0b8c82af6c623dd09113cdcd589a8b6
Leomssm <leonardomossim.redes@gmail.com>
2025-06-02 00:30:38 -0300

    async function getMails()

--- api/index.js
+++ api/index.js
@@ -56,14 +56,19 @@ app.get('/home', authenticate, async (req, res) => {
 
 app.get('/auth/google', (req, res) => {
     const redirect_uri = 'https://accounts.google.com/o/oauth2/v2/auth?' +
-        new URLSearchParams({
-            client_id: process.env.GOOGLE_CLIENT_ID,
-            redirect_uri: process.env.GOOGLE_REDIRECT_URI,
-            response_type: 'code',
-            scope: 'openid email profile https://www.googleapis.com/auth/gmail.readonly',
-            access_type: 'offline',
-            prompt: 'consent'
-        });
+    new URLSearchParams({
+        client_id: process.env.GOOGLE_CLIENT_ID,
+        redirect_uri: process.env.GOOGLE_REDIRECT_URI,
+        response_type: 'code',
+        scope: [
+          'openid',
+          'email',
+          'profile',
+          'https://www.googleapis.com/auth/gmail.readonly'
+        ].join(' '),
+        access_type: 'offline',
+        prompt: 'consent'
+    });
 
     res.redirect(redirect_uri);
 });
